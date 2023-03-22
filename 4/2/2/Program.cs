355f0589695355aa8f48f35958d8d9f212e237ea
LikOchka <shobekovaanzhelika@yandex.ru>
2023-03-22 19:24:20 +0300

    PRC

---
+++ 4/2/2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace prakta4_2
+{
+    class programma
+    {
+        static void Main(string[] args)
+        {
+            Pacage package_1 = new Pacage("Лёгкая посылка", 2);
+            Pacage package_2 = new Pacage("Утяжелённая посылка", 9);
+            sending_service sendingService = new sending_service();
+            sendingService.SendPackage(package_1);
+            sendingService.SendPackage(package_2);
+            Console.ReadKey(true);
+        }
+    }
+    class Pacage
+    {
+        public int Weight
+        {
+            get;
+            private set;
+        }
+        public string Pacage_Description
+        {
+            get;
+            private set;
+        }
+        public Pacage(string description, int weight)
+        {
+            Pacage_Description = description;
+            Weight = weight;
+        }
+    }
+    class sending_service
+    {
+        private static int LimitWeight;
+        private const int Limit = 12;
+        public void SendPackage(Pacage package)
+        {
+            LimitWeight += package.Weight;
+            if (LimitWeight >= Limit)
+            {
+                Console.WriteLine("Весь посылок слишком большой! Бан");
+            }
+            else
+            {
+                Console.WriteLine("{0} весом {1} фунтов успешно отправлена.", package.Pacage_Description, package.Weight);
+            }
+        }
+    }
+}
---
+++ 4/3/3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace prakta4_3
+{
+    class programma
+    {
+        static void Main(string[] args)
+        {
+            Orc orc_1 = new Orc();
+            Console.WriteLine(Orc.VsegoZolota);
+            Orc orc_2 = new Orc();
+            Orc orc_3 = new Orc();
+            Console.WriteLine(Orc.VsegoZolota);
+            Orc orc_4 = new Orc();
+            Orc orc_5 = new Orc();
+            Orc orc_6 = new Orc();
+            Console.WriteLine(orc_6.UkrZoloto);
+            Console.ReadKey(true);
+        }
+    }
+    class Orc
+    {
+        private static int count;
+        public static int VsegoZolota { get; private set; }
+        public int UkrZoloto { get; private set; }
+        public Orc()
+        {
+            count++;
+            VsegoZolota += 2;
+            if (count > 5)
+            {
+                UkrZoloto += 2;
+            }
+        }
+    }
+}
